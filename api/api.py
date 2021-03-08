e25134bd9fcc2bdaa22f7fa3021b9c5d2ff7b4bd
Siddharth Kale <siddharthkale97@gmail.com>
2021-03-07 18:22:02 -0700

    Pooling res q, implementation 1b

--- api/api.py
+++ api/api.py
@@ -5,6 +5,7 @@ import sys
 import boto3
 import json
 from decouple import config
+from timeit import default_timer as timer
 
 RESPONSE_QUEUE_NAME = 'TestQueue.fifo'
 
@@ -15,10 +16,17 @@ from image_classification import *
 UPLOAD_FOLDER = "D:/Codes/CloudComputingProject1/TestProject1/uploads"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-sqs = boto3.resource('sqs', region_name='us-east-1',
-        aws_access_key_id=config('ACCESS_ID'),
-        aws_secret_access_key=config('ACCESS_KEY'))
-queue = sqs.get_queue_by_name(QueueName=RESPONSE_QUEUE_NAME)
+# sqs = boto3.resource('sqs', region_name='us-east-1',
+#         aws_access_key_id=config('ACCESS_ID'),
+#         aws_secret_access_key=config('ACCESS_KEY'))
+
+#test code
+sqs = boto3.client('sqs', 
+                    region_name='us-east-1', 
+                    aws_access_key_id=config('ACCESS_ID'),
+                    aws_secret_access_key= config('ACCESS_KEY'))
+# test code ends
+# queue = sqs.get_queue_by_name(QueueName=RESPONSE_QUEUE_NAME)
 
 app = Flask(__name__)
 app.config["Debug"] = True
@@ -27,35 +35,86 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_filename(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/get_results', methods=['GET'])
+@app.route('/get_results/<total_files>', methods=['GET'])
 def get_res(total_files=None):
+    # Only for testing
+    if total_files == None or int(total_files)<=0:
+        total_files = 3
+    else:
+        total_files = int(total_files)
+    # Only for testing!!
     return render_template("uploaded_successfully.html", total_files=total_files)
 
-@app.route('/show_res', methods=['GET'])
-def show_res_now(total_files=None):
+@app.route('/show_res/<total_files>', methods=['GET'])
+def show_res_now(total_files):
     # Below 2 lines are just for testing!
-    if total_files == None:
-        total_files = 3
-    results = []
-    messages_to_delete = []
-    for message in queue.receive_messages(
-            MaxNumberOfMessages=total_files):
-        result = json.loads(message.body)
-        results.append(result)
-        messages_to_delete.append({
-            'Id': message.message_id,
-            'ReceiptHandle': message.receipt_handle
-        })
-        print(result)
-    still_processing_message = None
-    if len(results) != total_files:
-        still_processing_message = "Some files are still processing, please be patient!"
-        return render_template("show_result.html", results=results, still_processing_message=still_processing_message)
+    if total_files == None or int(total_files)<=0:
+        total_files = 4
     else:
-        # delete_response = queue.delete_messages(
-        #     Entries=messages_to_delete)
-        return render_template("show_result.html", results=results, still_processing_message=still_processing_message)
-    return "Epic Failure!"
+        total_files = int(total_files)
+    results = []
+    print("total files = ", total_files)
+    start_time = timer()
+    # test code here
+    response = sqs.get_queue_url(QueueName=RESPONSE_QUEUE_NAME)
+    queue_url = str(response['QueueUrl'])
+    end_time = timer()
+    while (len(results)<int(total_files)):
+        if (abs(end_time-start_time)>=60):
+            break
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=180,
+            WaitTimeSeconds=0
+        )
+
+        if 'Messages' in response:
+            message = response['Messages'][0]
+            receipt_handle = message['ReceiptHandle']
+            print(message)
+
+            sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=message['ReceiptHandle']
+            )
+            print('Received and deleted message: %s' % message)
+            message_body = json.loads(message['Body'])
+            results.append(message_body)
+            print("result is ",results)
+            print("len of results is ", len(results))
+
+        end_time = timer()
+        # print(end_time-start_time)
+    print("out")
+    print(results)
+    return render_template("show_result.html", results=results)
+    # test code ends here
+    # messages_to_delete = []
+    # for message in queue.receive_messages(
+    #         MaxNumberOfMessages=total_files):
+    #     result = json.loads(message.body)
+    #     results.append(result)
+    #     messages_to_delete.append({
+    #         'Id': message.message_id,
+    #         'ReceiptHandle': message.receipt_handle
+    #     })
+    #     print(result)
+    # still_processing_message = None
+    # if len(results) != total_files:
+    #     still_processing_message = "Some files are still processing, please be patient!"
+    #     return render_template("show_result.html", results=results, still_processing_message=still_processing_message)
+    # else:
+    #     # delete_response = queue.delete_messages(
+    #     #     Entries=messages_to_delete)
+    #     return render_template("show_result.html", results=results, still_processing_message=still_processing_message)
+    # return "Epic Failure!"
 
 # @app.route('/uploads/<filename>')
 # def uploaded_file(filename):
